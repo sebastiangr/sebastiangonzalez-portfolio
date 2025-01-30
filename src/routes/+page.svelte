a82b75247e595fa91b73700ce5e5fb8f9c2c163d
sebastiangr <sebastiangr@proton.me>
2025-01-30 12:18:52 -0500

    Update background color in main page to black

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="h-screen flex items-center justify-center bg-gray-950">
+<div class="h-screen flex items-center justify-center bg-black">
 	<div class="text-center text-white">
 		<h1 class="mb-4">SEBASTIÁN GONZÁLEZ RODRÍGUEZ</h1>
 		<h3>Testing...</h3>
