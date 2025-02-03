b4c449e20800d03a462901b641e82ada2419fe41
sebastiangr <sebastiangr@proton.me>
2025-02-02 23:09:36 -0500

    Add Google Fonts integration, update page titles, and enhance layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,22 @@
   import '../app.css';
 </script>
 
-<div class="bg-black text-white flex justify-center gap-4 p-4 fixed top-0 left-0 right-0 font-sans">
+<!-- <div class="bg-black text-white flex justify-center gap-4 p-4 fixed top-0 left-0 right-0 font-sans">
   <a href="/" class="text-white no-underline">Home</a>
   <a href="/portfolio" class="text-white no-underline">Portfolio</a>
   <a href="/contacto" class="text-white no-underline">Contacto</a>
-</div>
+</div> -->
 
-<div class="flex items-center justify-center min-h-screen">
+<div class="body-bg flex items-center justify-center min-h-screen">
   <slot />
 </div>
+
+
+<style>
+  .body-bg  {
+    /* background: linear-gradient(to bottom, #0f0f0f 0%, #2d2d2d 100%), linear-gradient(to top, rgb(15, 15, 15) 0%, rgba(204, 185, 185, 0.25) 100%); */
+    background: linear-gradient(to bottom, #2d2d2d 0%, rgb(19, 19, 19) 100%);
+    
+    background-blend-mode: multiply;  
+  }  
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,35 @@
+<script lang="ts">
+  import Logo from "$lib/assets/sebastiangonzalez.co-logo.svg";
+  import IconLinkedin from "$lib/assets/icon-linkedin.svg";
+  import IconWhatsapp from "$lib/assets/icon-whatsapp.svg";
+  import IconMail from "$lib/assets/icon-mail.svg";
+  import IconGithub from "$lib/assets/icon-github.svg";
+</script>
+
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css?family=Gelasio" rel="stylesheet">
+</svelte:head>
+
 <div class="text-center text-white">
-  <h1 class="text-4xl mb-4 font-sans">SEBASTIÁN GONZÁLEZ RODRÍGUEZ</h1>
-  <p class="font-sans animate-pulse">Testing...</p>
+
+  <img src={Logo} alt="Sebastian Gonzalez Logo" class="w-32 h-32 mx-auto mt-8 mb-4" />
+
+  <h1 class="text-2xl sm:text-4xl mb-4 uppercase">I'm Sebastián, <br>a freelancer web and app developer</h1>
+  <p class="font-sans animate-pulse">This web is under construction... come back soon</p>
+
+  <h2 class="text-xl sm:text-2xl mt-8 mb-4">GET IN TOUCH</h2>
+  <div class="flex justify-center">
+    <a href="mailto:sebastiangr@proton.me" target="_blank" rel="noopener noreferrer" class="m-4 transition duration-500 ease-in-out hover:scale-110">
+      <img src={IconMail} width="40" height="40" />
+    </a>
+    <a href="https://wa.me/573003060724" target="_blank" rel="noopener noreferrer" class="m-4 transition duration-500 ease-in-out hover:scale-110">
+      <img src={IconWhatsapp} width="40" height="40" />
+    </a>    
+    <a href="https://github.com/sebastiangr" target="_blank" rel="noopener noreferrer" class="m-4 transition duration-500 ease-in-out hover:scale-110">
+      <img src={IconGithub} width="40" height="40" />
+    </a> 
+    <a href="https://www.linkedin.com/in/sebastiangonzalez-co/" target="_blank" rel="noopener noreferrer" class="m-4 transition duration-500 ease-in-out hover:scale-110">
+      <img src={IconLinkedin} width="40" height="40" />
+    </a>
+  </div>
 </div>
